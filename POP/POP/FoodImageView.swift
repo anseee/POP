3ddc1cd32f4beed600efbca93d53e5c5b16f15bf
anseee <admin@ansee.kr>
2017-02-22 22:48:40 +0900

    FoodImageView.swift, ShakeableButton.swift, UIViewExtension.swift, project.pbxproj
    UIView extension 생성
    -> 기존 쉐이크 메서드를 extension으로 제작해서 UIView를 상속받고 있는 모든곳은 이 메서드를 사용할 수 있게 되었다.
    -> 기존 쉐이크 메서드를 구현하고 있던 뷰들은 모두 그 중복된 코드들을 제거한다.

--- POP/POP/FoodImageView.swift
+++ POP/POP/FoodImageView.swift
@@ -10,14 +10,5 @@ import UIKit
 
 class FoodImageView: UIImageView {
 
-    func shake() {
-        let animation = CABasicAnimation(keyPath: "position")
-        animation.duration = 0.05
-        animation.repeatCount = 5
-        animation.autoreverses = true
-        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 4.0, y: self.center.y))
-        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 4.0, y: self.center.y))
-        layer.add(animation, forKey: "position")
-    }
 
 }
