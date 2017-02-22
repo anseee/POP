beff8825540bf5a49dfc08b8e7ab7d2a02fefe43
anseee <admin@ansee.kr>
2017-02-22 22:41:06 +0900

    FoodImageView.swift
    커스텀 이미지 뷰 클래스 생성
    쉐이크 메서드 구현

---
+++ POP/POP/FoodImageView.swift
@@ -0,0 +1,23 @@
+//
+//  FoodImageView.swift
+//  POP
+//
+//  Created by 박성원 on 2017. 2. 22..
+//  Copyright © 2017년 sungwon. All rights reserved.
+//
+
+import UIKit
+
+class FoodImageView: UIImageView {
+
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 4.0, y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 4.0, y: self.center.y))
+        layer.add(animation, forKey: "position")
+    }
+
+}
